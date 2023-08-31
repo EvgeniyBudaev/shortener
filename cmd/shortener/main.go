d7512d651985888a4894d59ee7f14e9817e3e2fc
Evgeniy Budaev <budaev.e@gmail.com>
2023-08-31 18:19:42 +0300

    feat: increment 1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,34 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"strings"
+)
+
+func mainPage(res http.ResponseWriter, req *http.Request) {
+	host := req.Host
+	path := req.URL.Path
+	url := host + path
+	id := strings.TrimPrefix(req.URL.Path, "/")
+
+	if req.Method == http.MethodPost {
+		res.Header().Set("content-type", "text/plain")
+		res.WriteHeader(http.StatusCreated)
+		res.Write([]byte(url))
+	} else if id != "" && req.Method == http.MethodGet {
+		res.Header().Set("content-type", "text/plain")
+		res.WriteHeader(http.StatusTemporaryRedirect)
+	} else {
+		http.Error(res, "Bad Request", http.StatusBadRequest)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, mainPage)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/EvgeniyBudaev/shortener
+
+go 1.21.0
