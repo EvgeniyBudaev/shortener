4362d96760a38bdcd0e13eeacad177d994cb70e2
Evgeniy Budaev <budaev.e@gmail.com>
2023-09-01 06:53:01 +0300

    fix: mainPage

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,21 +1,48 @@
 package main
 
 import (
+	"crypto/rand"
+	"io"
+	"log"
+	"math/big"
 	"net/http"
 	"strings"
 )
 
+const httpProtocol = "http://"
+
 var urls = make(map[string][]byte)
 
-func mainPage(res http.ResponseWriter, req *http.Request) {
-	host := req.Host
-	path := req.URL.Path
-	url := host + path
+func GenerateRandomString(n int) (string, error) {
+	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	ret := make([]byte, n)
+	for i := 0; i < n; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			return "", err
+		}
+		ret[i] = letters[num.Int64()]
+	}
 
+	return string(ret), nil
+}
+
+func shortURL(res http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodPost {
+		body, err := io.ReadAll(req.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		id, err := GenerateRandomString(8)
+		if err != nil {
+			log.Fatal(err)
+		}
+		urls[id] = body
+
 		res.Header().Set("Content-Type", "text/plain")
 		res.WriteHeader(http.StatusCreated)
-		res.Write([]byte(url))
+		res.Write([]byte(httpProtocol + req.Host + "/" + id))
 
 		return
 	} else if req.Method == http.MethodGet {
@@ -32,7 +59,7 @@ func mainPage(res http.ResponseWriter, req *http.Request) {
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, mainPage)
+	mux.HandleFunc(`/`, shortURL)
 
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
