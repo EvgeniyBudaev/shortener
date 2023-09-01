951aa5cc8eb33b2e41fdfa35c5592fdabe678ea0
Evgeniy Budaev <budaev.e@gmail.com>
2023-09-01 07:01:04 +0300

    fix: mainPage

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"crypto/rand"
+	"io"
+	"log"
+	"math/big"
 	"net/http"
 	"strings"
 )
@@ -9,11 +13,36 @@ const httpProtocol = "http://"
 
 var urls = make(map[string][]byte)
 
+func GenerateRandomString(n int) (string, error) {
+	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	ret := make([]byte, n)
+	for i := 0; i < n; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			return "", err
+		}
+		ret[i] = letters[num.Int64()]
+	}
+
+	return string(ret), nil
+}
+
 func mainPage(res http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodPost {
+		body, err := io.ReadAll(req.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		id, err := GenerateRandomString(8)
+		if err != nil {
+			log.Fatal(err)
+		}
+		urls[id] = body
+
 		res.Header().Set("Content-Type", "text/plain")
 		res.WriteHeader(http.StatusCreated)
-		res.Write([]byte(httpProtocol + req.Host + "/"))
+		res.Write([]byte(httpProtocol + req.Host + "/" + id))
 
 		return
 	} else if req.Method == http.MethodGet {
