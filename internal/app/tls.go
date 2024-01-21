d09c2f420492ca7d3b8b40cbcc8d9c37a6056815
Evgeniy Budaev <budaev.e@gmail.com>
2024-01-21 08:31:30 +0300

    комментирование tls

--- internal/app/tls.go
+++ internal/app/tls.go
@@ -16,35 +16,56 @@ import (
 )
 
 const (
+	// serialNumber Уникальный серийный номер сертификата
 	serialNumber = 1
-	ip4GrayZone  = 127
-	yearsGrant   = 1
-	RSALen       = 4096
+	// ip4GrayZone IP-адрес, разрешенный для сертификата (в данном случае используется 127.0.0.1)
+	ip4GrayZone = 127
+	// yearsGrant Срок действия сертификата в годах
+	yearsGrant = 1
+	// RSALen Длина приватного ключа RSA в битах
+	RSALen = 4096
 )
 
-// CreateCertificates - создание сертификатов
+// CreateCertificates - создание TLS - сертификатов
 func CreateCertificates() error {
+	// создаём шаблон сертификата
 	cert := &x509.Certificate{
+		// указываем уникальный номер сертификата
 		SerialNumber: big.NewInt(serialNumber),
+		// заполняем базовую информацию о владельце сертификата
 		Subject: pkix.Name{
 			Organization: []string{"Shortener"},
 			Country:      []string{"RU"},
 		},
-		IPAddresses:  []net.IP{net.IPv4(ip4GrayZone, 0, 0, 1), net.IPv6loopback},
-		NotBefore:    time.Now(),
+		// разрешаем использование сертификата для 127.0.0.1 и ::1
+		IPAddresses: []net.IP{net.IPv4(ip4GrayZone, 0, 0, 1), net.IPv6loopback},
+		// сертификат верен, начиная со времени создания
+		NotBefore: time.Now(),
+		// время жизни сертификата — 10 лет
 		NotAfter:     time.Now().AddDate(yearsGrant, 0, 0),
 		SubjectKeyId: []byte{1, 2, 3, 4, 6},
-		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:     x509.KeyUsageDigitalSignature,
+		// устанавливаем использование ключа для цифровой подписи,
+		// а также клиентской и серверной авторизации
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:    x509.KeyUsageDigitalSignature,
 	}
+
+	// создаём новый приватный RSA-ключ длиной 4096 бит
+	// обратите внимание, что для генерации ключа и сертификата
+	// используется rand.Reader в качестве источника случайных данных
 	privateKey, err := rsa.GenerateKey(rand.Reader, RSALen)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// создаём сертификат x.509
 	certBytes, err := x509.CreateCertificate(rand.Reader, cert, cert, &privateKey.PublicKey, privateKey)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// кодируем сертификат и ключ в формате PEM, который
+	// используется для хранения и обмена криптографическими ключами
 	certFile, err := os.OpenFile("./certs/cert.pem", os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatal(err)
@@ -57,6 +78,7 @@ func CreateCertificates() error {
 	}); err != nil {
 		return fmt.Errorf("error creating cert file: %w", err)
 	}
+
 	rsaFile, err := os.OpenFile("./certs/private.pem", os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatal(err)
@@ -68,5 +90,6 @@ func CreateCertificates() error {
 	}); err != nil {
 		return fmt.Errorf("error creating RSA private key: %w", err)
 	}
+
 	return nil
 }
